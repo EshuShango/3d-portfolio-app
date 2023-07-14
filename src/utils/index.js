df69e977940fc2a0e931d616d0e439f666abb3fc
EshuShango <tobiakinyemi21@gmail.com>
2023-07-14 01:11:14 -0500

    fin prj section

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+
+import Loader from './Loader';
+// import motion from './motion';
+import { fadeIn, textVariant } from './motion';
+
+export default {
+ Loader,
+ motion,
+ fadeIn,
+  textVariant,  
+}
+//? How to make it work
